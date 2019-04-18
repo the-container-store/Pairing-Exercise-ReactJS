5d994017656b0b94383dadd358ee59c13928d52c
Nik Billings <nbillings@containerstore.com>
2019-04-18 17:41:37 -0500

    Fixed #2

--- src/components/StorageSummary/index.js
+++ src/components/StorageSummary/index.js
@@ -5,34 +5,33 @@ class StorageSummary extends Component {
   render() {
     const spaceId = this.props.spaceDocument.spaceId;
     const spaceUrl = `https://www.containerstore.com/custom-closets/space/view.htm?spaceId=${spaceId}`;
-    const amountOfShelves = storageCalculator.calculateShelvingLength(this.props.spaceDocument);
+    const userCalculations = storageCalculator.calculateShelvingLength(this.props.spaceDocument);
+    const totalShelves = Object.values(userCalculations).reduce((fin, stuff) => {
+      const a = stuff.shelves
+      return fin + a
+    }, 0)
+
     return (
       <div>
         <h1>Storage Summary <a href={spaceUrl}>#{spaceId}</a></h1>
         <div className="user">
-          <h2>{this.props.spaceDocument.design.users[0].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
+          {this.props.spaceDocument.design.users.map(user => (
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+              <ul>
+                <li>Shelves: {userCalculations[user.id].shelves} inches</li>
+                <li>Hanging: ? inches</li>
+                <li>Shoes: ? shoes</li>
+                <li>Drawers: ? </li>
+              </ul>
+            </div>
+          ))}
+
         </div>
-        {this.props.spaceDocument.design.users[1] &&
-        <div className="user">
-          <h2>{this.props.spaceDocument.design.users[1].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
-        </div>
-        }
         <div className="user">
           <h2>Total</h2>
           <ul>
-            <li>Shelves: ? inches</li>
+            <li>Shelves: {totalShelves} inches</li>
             <li>Hanging: ? inches</li>
             <li>Shoes: ? shoes</li>
             <li>Drawers: ? </li>
--- src/components/StorageSummary/storageCalculator.js
+++ src/components/StorageSummary/storageCalculator.js
@@ -1,16 +1,42 @@
+const isRelated = (sections) => {
+  const ids = sections.map(section => section.id)
+
+  return (shelf) => {
+    return ids.includes(shelf.sectionId)
+  }
+}
+
 const calculateShelvingLength = (spaceDocument) => {
-  const sectionWidthMap = {};
-  spaceDocument.design.zones.forEach(zone => {
-    zone.sections.forEach(section => {
-      sectionWidthMap[section.id] = section.width;
-    });
-  });
-  const shelves = spaceDocument.design.elements.filter(element => element.type.includes('shelf'));
-  let totalLength = 0;
-  shelves.forEach(shelf => {
-    totalLength += sectionWidthMap[shelf.sectionId];
-  });
-  return Math.round(totalLength / 25.4);
+
+  const { users, zones, elements } = spaceDocument.design
+
+  const userStuff = users.reduce((final, user) => {
+    const uid = user.id
+
+    const sections = zones.map(zone => {
+      return zone.sections
+    }).flat()
+
+    const userSections = sections.filter(section => {
+      return  uid === section.userId
+    })
+
+    const checkRelation = isRelated(userSections)
+    const shelves = elements.filter(element => element.type.includes('shelf') && checkRelation(element) )
+
+    const shelfWidths = shelves.reduce((fin, shelf) => {
+      const foo = userSections.find(section => section.id === shelf.sectionId).width
+      return fin += foo
+    }, 0)
+
+    final[user.id] = {
+      shelves: Math.round(shelfWidths / 25.4), 
+    }
+
+    return final
+  }, {})
+
+  return userStuff
 }
 
 export default { calculateShelvingLength };
