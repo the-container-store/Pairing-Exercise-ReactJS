ec490a580b3b79af0c6e4bcb27e0fcb2a4e3c0c0
Andy Asberry <faasberry@containerstore.com>
2019-04-24 15:00:24 -0500

    Pairing work with Mario

--- src/components/StorageSummary/index.js
+++ src/components/StorageSummary/index.js
@@ -7,27 +7,30 @@ class StorageSummary extends Component {
     const spaceId = this.props.spaceDocument.spaceId;
     const spaceUrl = `https://www.containerstore.com/custom-closets/space/view.htm?spaceId=${spaceId}`;
     const amountOfShelves = storageCalculator.calculateShelvingLength(this.props.spaceDocument);
+    const summaryList = (shelves) =>{
+      return(
+          <ul>
+            <li>Shelves: {shelves} inches</li>
+            <li>Hanging: ? inches</li>
+            <li>Shoes: ? shoes</li>
+            <li>Drawers: ? </li>
+          </ul>
+      )
+      
+    }
+
     return (
       <div>
         <h1>Storage Summary <a href={spaceUrl}>#{spaceId}</a></h1>
         <div className="user">
           <h2>{this.props.spaceDocument.design.users[0].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
+          {summaryList(amountOfShelves)}
+        
         </div>
         {this.props.spaceDocument.design.users[1] &&
         <div className="user">
           <h2>{this.props.spaceDocument.design.users[1].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
+          {summaryList(amountOfShelves)}
         </div>
         }
         <div className="user">
