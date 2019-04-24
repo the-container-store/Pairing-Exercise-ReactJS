5dd5a79900f94c60e89f99c8da029a32dbacc9a7
Nik Billings <nbillings@containerstore.com>
2019-04-24 16:37:22 -0500

    refactored index.js , added user totals

--- src/components/StorageSummary/index.js
+++ src/components/StorageSummary/index.js
@@ -7,41 +7,36 @@ class StorageSummary extends Component {
     const spaceId = this.props.spaceDocument.spaceId;
     const spaceUrl = `https://www.containerstore.com/custom-closets/space/view.htm?spaceId=${spaceId}`;
     const amountOfShelves = storageCalculator.calculateShelvingLength(this.props.spaceDocument);
-    const summaryList = (shelves) =>{
+    console.log(amountOfShelves)
+
+    const summaryList = (user,shelves) =>{
       return(
+        <div className="user">
+        <h2>{user}</h2>
           <ul>
             <li>Shelves: {shelves} inches</li>
             <li>Hanging: ? inches</li>
             <li>Shoes: ? shoes</li>
             <li>Drawers: ? </li>
           </ul>
+          </div>
       )
-      
     }
+
+    let totalShelves = 0;
+    amountOfShelves.forEach(user=>{totalShelves+=user.totalShelves})
 
     return (
       <div>
         <h1>Storage Summary <a href={spaceUrl}>#{spaceId}</a></h1>
-        <div className="user">
-          <h2>{this.props.spaceDocument.design.users[0].name}</h2>
-          {summaryList(amountOfShelves)}
-        
-        </div>
-        {this.props.spaceDocument.design.users[1] &&
-        <div className="user">
-          <h2>{this.props.spaceDocument.design.users[1].name}</h2>
-          {summaryList(amountOfShelves)}
-        </div>
-        }
-        <div className="user">
-          <h2>Total</h2>
-          <ul>
-            <li>Shelves: ? inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
-        </div>
+       
+          {
+            amountOfShelves.map(user=>summaryList(user.userName,user.totalShelves)            )
+          }
+      
+          {summaryList("Total",totalShelves)}
+
+    
       </div>
     );
   }
--- src/components/StorageSummary/storageCalculator.js
+++ src/components/StorageSummary/storageCalculator.js
@@ -1,16 +1,69 @@
+// const calculateShelvingLength = (spaceDocument) => {
+//   const sectionWidthMap = {};
+//   spaceDocument.design.zones.forEach(zone => {
+//     zone.sections.forEach(section => {
+//       sectionWidthMap[section.id] = section.width;
+//     });
+//   });
+//   const shelves = spaceDocument.design.elements.filter(element => element.type.includes('shelf'));
+//   let totalLength = 0;
+//   shelves.forEach(shelf => {
+//     totalLength += sectionWidthMap[shelf.sectionId];
+//   });
+//   return Math.round(totalLength / 25.4);
+// }
+
+
+
 const calculateShelvingLength = (spaceDocument) => {
-  const sectionWidthMap = {};
-  spaceDocument.design.zones.forEach(zone => {
-    zone.sections.forEach(section => {
-      sectionWidthMap[section.id] = section.width;
-    });
-  });
-  const shelves = spaceDocument.design.elements.filter(element => element.type.includes('shelf'));
-  let totalLength = 0;
-  shelves.forEach(shelf => {
-    totalLength += sectionWidthMap[shelf.sectionId];
-  });
-  return Math.round(totalLength / 25.4);
+  const users = spaceDocument.design.users;
+  const elements = spaceDocument.design.elements;
+  let sections = [];
+  users.forEach(user=>{
+    let userSections = [];
+
+    spaceDocument.design.zones.forEach(zone=>{
+      zone.sections.forEach(section => 
+        {
+          if(section.userId === user.id){
+            userSections.push(section)
+          }
+        }
+        )
+    })
+
+
+
+    
+      sections.push({userName:user.name,userId:user.id,userSections:userSections})
+  })
+
+  let elementLengths = [];
+  sections.forEach(section =>{
+    let total = 0
+    elements.forEach(element => {
+      if(section.id === element.sectionId && element.type.includes("shelf")){
+        total += section.width;
+      }
+    })
+  })
+
+  sections.forEach(section => {
+    let total = 0
+    section.userSections.forEach(userSection =>{
+      elements.forEach(element => {
+        if(userSection.id === element.sectionId && element.type.includes("shelf")){
+          total += userSection.width;
+        }
+      })
+
+    })
+    section.totalShelves = Math.round(total / 25.4);
+  })
+
+  return sections
+
 }
 
 export default { calculateShelvingLength };
+
