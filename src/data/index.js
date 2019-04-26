536ef8d33eb0a915feff2fdf71c78107d89c59f9
Andy Asberry <faasberry@containerstore.com>
2019-04-26 15:37:55 -0500

    Andy - add task list and setup

---
+++ src/data/index.js
@@ -0,0 +1,29 @@
+import spaceDocument1 from "./space_5148448";
+import spaceDocument2 from "./space_5148457";
+import spaceDocument3 from "./space_5148443";
+
+export default {
+  space1: {
+    spaceDocument: spaceDocument1,
+    imageUrls: [
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178196&type=DESIGN&name=Floor",
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?name=Wall+A&fileId=7178196&type=DESIGN"
+    ]
+  },
+  space2: {
+    spaceDocument: spaceDocument2,
+    imageUrls: [
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Floor",
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Wall+A"
+    ]
+  },
+  space3: {
+    spaceDocument: spaceDocument3,
+    imageUrls: [
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Floor",
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+D",
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+A",
+      "https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+B"
+    ]
+  }
+};
---
+++ tasks-ui.md
@@ -0,0 +1,27 @@
+# Tasks
+
+ - [ ] Clean up App.js (5 mins)
+ - [ ] Add styling to Storage Summary (5-10 mins)
+ - [ ] Dry up StorageSummary component (10 mins)
+ - [ ] Issue PR (you can use `git publish` on this machine)
+
+## Clean up App.js
+
+ - [ ] Single handler for clicking button
+
+## Add styling to Storage Summary
+
+ - [ ] Remove bullet points
+ - [ ] Add border around storage summary "cards"
+ - [ ] Spacing between cards
+ - [ ] Left align card content
+
+## Improve StorageSummary component
+
+What do you think should be done? Can you implement it, while explaining?
+
+## NOTES
+
+### Colors Used
+
+ - grey: #F6F8FA
