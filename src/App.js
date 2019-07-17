e48347592d5a87654ad081383e2ed2061e26b0fc
Andy Asberry <faasberry@containerstore.com>
2019-07-17 11:09:32 -0500

    cleanup and styling

--- src/App.js
+++ src/App.js
@@ -1,67 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import StorageSummary from './components/StorageSummary';
 import spaceDocument1 from './data/space_5148448';
 import spaceDocument2 from './data/space_5148457';
 import spaceDocument3 from './data/space_5148443';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spaceDocument: spaceDocument1,
-      imageUrls: [
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178196&type=DESIGN&name=Floor',
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?name=Wall+A&fileId=7178196&type=DESIGN',
-      ],
-    };
+const App = ()=> {
+
+  const[spaceDocument, setStateDocument]=useState(spaceDocument1);
+  const[imageUrls, setImageUrls]=useState([
+    'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178196&type=DESIGN&name=Floor',
+    'https://www.containerstore.com/elfa/wall/imageContent.htm?name=Wall+A&fileId=7178196&type=DESIGN',
+  ])
+
+  const button1Clicked = () => {
+    setStateDocument(spaceDocument1);
+    setImageUrls([
+      'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178196&type=DESIGN&name=Floor',
+      'https://www.containerstore.com/elfa/wall/imageContent.htm?name=Wall+A&fileId=7178196&type=DESIGN',
+    ]);
   }
-  render() {
-    return (
-      <div className="App">
-        <div>
-          <button onClick={this.button1Clicked}>Space 1</button>
-          <button onClick={this.button2Clicked}>Space 2</button>
-          <button onClick={this.button3Clicked}>Space 3</button>
-        </div>
-        <StorageSummary spaceDocument={this.state.spaceDocument} />
-        <div className="images">
-          {this.state.imageUrls.map(url => 
-            (<img src={url} alt="Space" width={(100 / this.state.imageUrls.length) + '%'}/>)  
-          )}
-        </div>
-      </div>
-    );
+  const button2Clicked = () => {
+    setStateDocument(spaceDocument2);
+    setImageUrls([
+      'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Floor',
+      'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Wall+A',
+    ]);
+    
   }
-  button1Clicked = () => {
-    this.setState({
-      spaceDocument: spaceDocument1,
-      imageUrls: [
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178196&type=DESIGN&name=Floor',
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?name=Wall+A&fileId=7178196&type=DESIGN',
-      ],
-    });
-  }
-  button2Clicked = () => {
-    this.setState({
-      spaceDocument: spaceDocument2,
-      imageUrls: [
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Floor',
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178808&type=DESIGN&name=Wall+A',
-      ],
-    });
-  }
-  button3Clicked = () => {
-    this.setState({
-      spaceDocument: spaceDocument3,
-      imageUrls: [
-        'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Floor',
+  const button3Clicked = () => {
+    setStateDocument(spaceDocument3);
+    setImageUrls([
+      'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Floor',
         'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+D',
         'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+A',
         'https://www.containerstore.com/elfa/wall/imageContent.htm?fileId=7178188&type=DESIGN&name=Wall+B',
-      ],
-    });
+    ]);
+
   }
+  return (
+    <div className="App">
+      <div>
+        <button onClick={button1Clicked}>Space 1</button>
+        <button onClick={button2Clicked}>Space 2</button>
+        <button onClick={button3Clicked}>Space 3</button>
+      </div>
+      <StorageSummary spaceDocument={spaceDocument} />
+      <div className="images">
+        {imageUrls.map(url => 
+          (<img src={url} alt="Space" width={(100 / imageUrls.length) + '%'}/>)  
+        )}
+      </div>
+    </div>
+  );
+ 
 }
 
 export default App;
--- src/components/StorageSummary/index.js
+++ src/components/StorageSummary/index.js
@@ -1,46 +1,53 @@
-import React, { Component } from 'react';
-import storageCalculator from './storageCalculator';
+import React from "react";
+import storageCalculator from "./storageCalculator";
 
-class StorageSummary extends Component {
-  render() {
-    const spaceId = this.props.spaceDocument.spaceId;
-    const spaceUrl = `https://www.containerstore.com/custom-closets/space/view.htm?spaceId=${spaceId}`;
-    const amountOfShelves = storageCalculator.calculateShelvingLength(this.props.spaceDocument);
-    return (
-      <div>
-        <h1>Storage Summary <a href={spaceUrl}>#{spaceId}</a></h1>
-        <div className="user">
-          <h2>{this.props.spaceDocument.design.users[0].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
-        </div>
-        {this.props.spaceDocument.design.users[1] &&
-        <div className="user">
-          <h2>{this.props.spaceDocument.design.users[1].name}</h2>
-          <ul>
-            <li>Shelves: {amountOfShelves} inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
-        </div>
-        }
-        <div className="user">
-          <h2>Total</h2>
-          <ul>
-            <li>Shelves: ? inches</li>
-            <li>Hanging: ? inches</li>
-            <li>Shoes: ? shoes</li>
-            <li>Drawers: ? </li>
-          </ul>
-        </div>
+const StorageSummary = ({spaceDocument}) => {
+  const {spaceId, design}=spaceDocument
+
+  const spaceUrl = `https://www.containerstore.com/custom-closets/space/view.htm?spaceId=${spaceId}`;
+  const amountOfShelves = storageCalculator.calculateShelvingLength(
+    spaceDocument
+  );
+  
+  return (
+    <div>
+      <h1>
+        Storage Summary <a href={spaceUrl}>#{spaceId}</a>
+      </h1>
+      <div className="user">
+        <h2 className='user-header'>{design.users[0].name}</h2>
+        <hr />
+        <ul className='user-space'>
+          <li className='user-space-entry'>Shelves: {amountOfShelves} inches</li>
+          <li className='user-space-entry'>Hanging: ? inches</li>
+          <li className='user-space-entry'>Shoes: ? shoes</li>
+          <li className='user-space-entry'>Drawers: ? </li>
+        </ul>
       </div>
-    );
-  }
-}
+      {design.users[1] && (
+        <div className="user">
+          <h2 className="user-header">{design.users[1].name}</h2>
+          <hr />
+          <ul className="user-space">
+            <li className="user-space-entry">Shelves: {amountOfShelves} inches</li>
+            <li className="user-space-entry">Hanging: ? inches</li>
+            <li className="user-space-entry">Shoes: ? shoes</li>
+            <li className="user-space-entry">Drawers: ? </li>
+          </ul>
+        </div>
+      )}
+      <div className="user">
+        <h2 className='user-header'>Total</h2>
+        <hr />
+        <ul className='user-space'>
+          <li className='user-space-entry'>Shelves: ? inches</li>
+          <li className='user-space-entry'>Hanging: ? inches</li>
+          <li className='user-space-entry'>Shoes: ? shoes</li>
+          <li className='user-space-entry'>Drawers: ? </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default StorageSummary;
